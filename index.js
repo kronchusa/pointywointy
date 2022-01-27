f8b6ae8b71def54767855c7b46cc24b1bc377ac4
davidsheffield <david.jonathan.sheffield@gmail.com>
2022-01-26 23:20:49 -0500

    Changing port

--- index.js
+++ index.js
@@ -71,7 +71,8 @@ io.on('connection', (socket) => {
       });
   });
   
+let port = process.env.PORT || 80
 
-server.listen(3000, () => {
+server.listen(port, () => {
   console.log('listening on *:3000');
 });
